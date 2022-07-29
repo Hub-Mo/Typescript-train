71ba42a8abfc0f20a46ca93205afe82437406e89
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-29 14:37:11 +0200

    filtered all radio props and methods from car class and renamed them

--- SOLID/0.S/radio.js
+++ SOLID/0.S/radio.js
@@ -1,21 +1,18 @@
-
 export class Radio {
-    private _musicLevel : number = 0;
-    private _oldMusicLevel : number = 50;
-
-    get musicLevel(): number {
+    constructor() {
+        this._musicLevel = 0;
+        this._oldMusicLevel = 50;
+    }
+    get musicLevel() {
         return this._musicLevel;
     }
-
-    set musicLevel(value: number) {
+    set musicLevel(value) {
         this._musicLevel = value;
         this._oldMusicLevel = value;
     }
-
     turnMusicOn() {
         this._musicLevel = this._oldMusicLevel;
     }
-
     turnMusicOff() {
         this._musicLevel = 0;
     }
